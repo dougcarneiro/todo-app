044c9a7c70b6f3e3e0c8443e880b6c8f82f4f02b
Douglas Carneiro <douglasefcarneiro@gmail.com>
2023-11-14 14:14:51 -0300

    corrigindo seed

--- js/data/seed.js
+++ js/data/seed.js
@@ -1,42 +1,38 @@
-// export const seed = [
-//     {
-//       id: 'b9f2414d-b8dd-484d-8179-83383d10a3fd',
-//       title: 'Exemplo de Afazer 1',
-//       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante orci, ultricies ut placerat vel, fermentum quis leo. Sed at iaculis erat, in sodales urna. Duis pellentesque purus sed lacus viverra, tincidunt hendrerit ante ultrices.',
-//       priority: 'light',
-//       is_completed: 'false',
-//       date: '2023-08-21T00:00:00-03:00',
-//       created_at: '2023-08-20T00:00:00-03:00',
-//     },
-//     {
-//       id: 'bb0a4c9b-8eea-4d79-a248-39c8b846fa9e',
-//       title: 'Exemplo de Afazer 2',
-//       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante orci, ultricies ut placerat vel, fermentum quis leo. Sed at iaculis erat, in sodales urna. Duis pellentesque purus sed lacus viverra, tincidunt hendrerit ante ultrices.',
-//       priority: 'medium',
-//       is_completed: 'false',
-//       date: '2023-08-23T00:00:00-03:00',
-//       created_at: '2023-08-21T00:00:00-03:00',
-//     },
-//     {
-//       id: '4b4cea63-f3bc-49b2-8b9a-750908d7a216',
-//       title: 'Exemplo de Afazer 3',
-//       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante orci, ultricies ut placerat vel, fermentum quis leo. Sed at iaculis erat, in sodales urna. Duis pellentesque purus sed lacus viverra, tincidunt hendrerit ante ultrices.',
-//       priority: 'high',
-//       is_completed: 'false',
-//       date: '2023-08-24T00:00:00-03:00',
-//       created_at: '2023-08-22T00:00:00-03:00',
-//     },
-//     {
-//       id: 'ac8129d9-147d-4cfb-a7a3-b4728da82334',
-//       title: 'Exemplo de Afazer 4',
-//       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante orci, ultricies ut placerat vel, fermentum quis leo. Sed at iaculis erat, in sodales urna. Duis pellentesque purus sed lacus viverra, tincidunt hendrerit ante ultrices.',
-//       priority: 'normal',
-//       is_completed: 'true',
-//       date: '2023-08-24T00:00:00-03:00',
-//       created_at: '2023-08-22T00:00:00-03:00',
-//     },
-//   ];
-
 export const seed = [
- 
-];
+    {
+      id: 'b9f2414d-b8dd-484d-8179-83383d10a3fd',
+      title: 'Exemplo de Afazer 1',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante orci, ultricies ut placerat vel, fermentum quis leo. Sed at iaculis erat, in sodales urna. Duis pellentesque purus sed lacus viverra, tincidunt hendrerit ante ultrices.',
+      priority: 'light',
+      is_completed: false,
+      date: '2023-08-21T00:00:00-03:00',
+      created_at: '2023-08-20T00:00:00-03:00',
+    },
+    {
+      id: 'bb0a4c9b-8eea-4d79-a248-39c8b846fa9e',
+      title: 'Exemplo de Afazer 2',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante orci, ultricies ut placerat vel, fermentum quis leo. Sed at iaculis erat, in sodales urna. Duis pellentesque purus sed lacus viverra, tincidunt hendrerit ante ultrices.',
+      priority: 'medium',
+      is_completed: false,
+      date: '2023-08-23T00:00:00-03:00',
+      created_at: '2023-08-21T00:00:00-03:00',
+    },
+    {
+      id: '4b4cea63-f3bc-49b2-8b9a-750908d7a216',
+      title: 'Exemplo de Afazer 3',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante orci, ultricies ut placerat vel, fermentum quis leo. Sed at iaculis erat, in sodales urna. Duis pellentesque purus sed lacus viverra, tincidunt hendrerit ante ultrices.',
+      priority: 'high',
+      is_completed: false,
+      date: '2023-08-24T00:00:00-03:00',
+      created_at: '2023-08-22T00:00:00-03:00',
+    },
+    {
+      id: 'ac8129d9-147d-4cfb-a7a3-b4728da82334',
+      title: 'Exemplo de Afazer 4',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ante orci, ultricies ut placerat vel, fermentum quis leo. Sed at iaculis erat, in sodales urna. Duis pellentesque purus sed lacus viverra, tincidunt hendrerit ante ultrices.',
+      priority: 'normal',
+      is_completed: true,
+      date: '2023-08-24T00:00:00-03:00',
+      created_at: '2023-08-22T00:00:00-03:00',
+    },
+  ];
